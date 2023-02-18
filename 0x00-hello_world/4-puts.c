ce0413928ecea62cba389134051e3c984fd4363a
Wachera Waweru <wacheram22@gmail.com>
2023-02-18 12:12:08 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts
+
+/**
+ * main -Entry point of the program
+ *
+ * print a line of code using puts
  *
  * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
+
