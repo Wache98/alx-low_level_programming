a5b01f5da57c508bfba7ec43ffacdf75d058c7b7
Wachera Waweru <wacheram22@gmail.com>
2023-02-22 03:00:14 -0800

    prints lowercase alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,8 @@ void print_alphabet_x10(void)
 	char letter;
 
 	while (count++ <= 9)
-
-	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	_putchar('/n');
+	_putchar('\n');
 }
